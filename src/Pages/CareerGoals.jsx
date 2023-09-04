f4ea3155b980c763e1ae2386c4b33ceb282dc310
RishinKrishna <rishinkrishna07@gmail.com>
2023-09-04 17:47:49 +0530

    career goals page content added.

--- src/Pages/CareerGoals.jsx
+++ src/Pages/CareerGoals.jsx
@@ -1,28 +1,87 @@
 // src/components/CareerGoals.jsx
 import React from "react";
-import { Navbar, Footer } from "../components";
+import { Navbar, Footer, Hero } from "../components";
 import styles from "../style";
+import { careerRequirements, careerObjectives } from "../constants";
 const CareerGoals = () => {
   return (
-    <div>
-      <div className={`bg-slate-500 ${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth} `}>
-          <Navbar />
+    <div className="bg-[#F0F0F0]">
+      <div className="w-fill h-screen">
+        <div
+          className="w-full   h-screen bg-top bg-no-repeat  bg-cover bg-fixed overflow-hidden careee_main_image"
+          // style={{
+          //   backgroundImage: `url("https://wallpapercave.com/wp/wp2722874.jpg")`
+          // }}
+        >
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth} `}>
+              <Navbar />
+            </div>
+          </div>
+          <div className="w-full h-full flex justify-center items-center">
+            <h2
+              className={`text-[#fefefe] flex justify-center   font-poppins font-semibold text-[75px] text-center py-10`}
+            >
+              CAREER GOALS
+            </h2>
+          </div>
         </div>
       </div>
 
-      
       <h2
-        className={`text-[#141824] flex justify-center font-poppins font-semibold text-4xl py-10`}
+        className={`text-[#141824] flex justify-center text-center font-poppins font-semibold text-4xl py-12`}
       >
-        CAREER GOALS
+        Key Requirements for My Next Role
       </h2>
-
 
       {/* Your content for career goals */}
 
+      <div className={` ${styles.paddingX} ${styles.flexStart}`}>
+        <div className="services_container grid">
+          {careerRequirements.map(({ id, image, title, description }) => {
+            return (
+              <div
+                className="career_requirements_card flex flex-col justify-start items-center mb-7"
+                key={id}
+              >
+                <img src={image} alt="" className="w-[65px]  items-center" />
+                <h3 className="services_title text-[22px] mt-3 font-semibold text-[#8b88b1] hover:text-[#990033]">
+                  {title}
+                </h3>
+                <p className="services_description text-[14px] mt-2 ">
+                  {description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <h2
+        className={`text-[#141824] flex justify-center font-poppins font-semibold text-4xl py-10`}
+      >
+        Career Objectives
+      </h2>
 
-
+      <div className={` ${styles.paddingX} ${styles.flexStart}`}>
+        <div className="services_container grid">
+          {careerObjectives.map(({ id, image, title, description }) => {
+            return (
+              <div
+                className="career_objectives_card flex flex-col justify-center items-center mb-7"
+                key={id}
+              >
+                <img src={image} alt="" className="w-[65px]  items-center" />
+                <h3 className="services_title text-[22px] mt-3 font-semibold text-[#8b88b1] hover:text-[#990033]">
+                  {title}
+                </h3>
+                <p className="services_description text-[14px] mt-2 ">
+                  {description}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <div className={`bg-[#141824] ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Footer />
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,19 @@ import {
   consulting_img,
   implementation_img,
   tech_Support_img,
+  work_mode_img,
+  role_preference_img,
+  project_management_img,
+  consultative_aproach_img,
+  working_hours_img,
+  afternoon_constraints_img,
+  compensation_structure_img,
+  passion_and_purpose_img,
+  crystal_carity_img,
+  innovation_and_strategy_img,
+  consultation_and_respect_img,
+  freedom_and_scalability_img,
+  personal_time_and_family_moments_img
 } from "../assets";
 
 export const serviceCard = [
@@ -30,36 +43,36 @@ export const serviceCard = [
 
 export const navLinks = [
   {
-    id: "#home",
+    id: "/#home",
     title: "Home",
   },
   {
-    id: "#about",
+    id: "/#about",
     title: "About",
   },
   {
-    id: "#services",
+    id: "/#services",
     title: "Services",
   },
   {
-    id: "#experience",
+    id: "/#experience",
     title: "Experience",
   },
 
   {
-    id: "#portfolio",
+    id: "/#portfolio",
     title: "Portfolio",
   },
   {
-    id: "#skills",
+    id: "/#skills",
     title: "Skills",
   },
   {
-    id: "#clinets",
+    id: "/#clinets",
     title: "Clinets",
   },
   {
-    id: "#contact",
+    id: "/#contact",
     title: "Contact",
   },
   {
@@ -155,3 +168,101 @@ export const footerLinks = [
     ],
   },
 ];
+
+export const careerRequirements = [
+  {
+    id: 1,
+    image: work_mode_img,
+    title: "Work Mode",
+    description:
+      "I'm seeking positions that offer remote or hybrid working arrangements with defined time constraints.",
+  },
+  {
+    id: 2,
+    image: role_preference_img,
+    title: "Role Preference",
+    description:
+      " My expertise lies in architectural design. I thrive as a solution provider and prefer this over routine tasks like coding, documentation, and bug fixes.",
+  },
+  {
+    id: 3,
+    image: project_management_img,
+    title: "Project Management",
+    description:
+      " I excel in initiating and developing projects from scratch within set timelines and budgets. However, adapting to existing systems or code without comprehensive documentation is challenging for me.",
+  },
+  {
+    id: 4,
+    image: consultative_aproach_img,
+    title: "Consultative Approach",
+    description:
+      "I aim for a consulting role where I can harness my full potential, sincerity, and dedication, instead of a traditional employment model. My ideal work duration is 4-5 hours daily, not exceeding 100 hours monthly.",
+  },
+  {
+    id: 5,
+    image: working_hours_img,
+    title: "Working Hours",
+    description:
+      "My productivity peaks during early morning shifts in the Indian timezone. If the job requires an 8-hour workday, I'd prefer flexible hours to maximize my efficiency.",
+  },
+  {
+    id: 6,
+    image: afternoon_constraints_img,
+    title: "Afternoon Constraints",
+    description:
+      "I have personal commitments that make me unavailable post 1:00 PM IST, but I am readily available in the early mornings.",
+  },
+  {
+    id: 7,
+    image: compensation_structure_img,
+    title: "Compensation Structure",
+    description:
+      "If considering a permanent role, please note I operate my consultation agency and manage my insurance and funds (NPF & PPF). I would appreciate an hourly compensation without additional benefits (Optional).",
+  },
+];
+
+
+export const careerObjectives = [
+  {
+    id: 1,
+    image: passion_and_purpose_img,
+    title: "Passion and Purpose",
+    description:
+      "I aspire to immerse myself in roles where passion meets purpose. Every task I undertake should resonate with my core values, ensuring I approach my work with enthusiasm and a fresh mindset daily. Monotony and fatigue should be strangers to my professional journey.",
+  },
+  {
+    id: 2,
+    image: crystal_carity_img,
+    title: "Crystal Clarity",
+    description:
+      " Comprehensive understanding is paramount. I desire to be involved in systems and architectures where every activity, no matter how minute, is transparent. This not only helps in efficiently navigating the complexities but also ensures that my contributions feel justified and purposeful.",
+  },
+  {
+    id: 3,
+    image: innovation_and_strategy_img,
+    title: "Innovation and Strategy",
+    description:
+      "I am driven by the power of research and development. The freedom to implement my unique ideas and strategies is crucial. I aim to perpetually refine processes, enhancing productivity, efficiency, and profitability. Establishing streamlined operational systems remains a top priority.",
+  },
+  {
+    id: 4,
+    image: consultation_and_respect_img,
+    title: "Consultation and Respect",
+    description:
+      "My interactions should be characterized by mutual respect. I visualize a consulting model for my professional engagements, where my contributions are not just acknowledged but valued. Any engagement I undertake should factor in covering my personal expenses, ensuring a fair trade of services.",
+  },
+  {
+    id: 5,
+    image: freedom_and_scalability_img,
+    title: "Freedom and Scalability",
+    description:
+      "Time and financial management should be under my control, granting me the liberty to design my work-life balance. My professional model should be scalable, leveraging my established personal brand for future ventures and opportunities. I am inclined towards straightforward, complication-free endeavors.",
+  },
+  {
+    id: 6,
+    image: personal_time_and_family_moments_img,
+    title: "Personal Time and Family Moments",
+    description:
+      "After dedicating a decade to professional pursuits, it's imperative for me to reclaim lost moments. I need the flexibility to pause, to breathe, and prioritize family and personal life. Responsibilities towards my loved ones are paramount, and I wish to balance them alongside my professional commitments.",
+  },
+]
