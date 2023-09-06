947a138b365ef691591b7ee222ef396a11b74727
Anand VM <anandnambissan2@gmail.com>
2023-09-06 18:13:46 +0530

    Updated goals page2

--- src/Pages/CareerGoals.jsx
+++ src/Pages/CareerGoals.jsx
@@ -31,24 +31,10 @@ const CareerGoals = () => {
       <h2
         className={`text-[#141824] flex justify-center text-center font-poppins font-semibold text-4xl py-12`}
       >
-        Key Requirements for my next Role
+        Key Requirements for My Next Role
       </h2>
 
       {/* Your content for career goals */}
-      <div className={`py-8 px-5`}>
-        <h3 className={`text-2xl font-semibold mb-5`}>
-          Key Requirements for My Next Role
-        </h3>
-        <ol className={`list-decimal pl-5`}>
-          <li>Work Mode: I'm seeking positions that offer remote or hybrid working arrangements with defined time constraints.</li>
-          <li>Role Preference: My expertise lies in architectural design. I thrive as a solution provider and prefer this over routine tasks like coding, documentation, and bug fixes.</li>
-          <li>Project Management: I excel in initiating and developing projects from scratch within set timelines and budgets. However, adapting to existing systems or code without comprehensive documentation is challenging for me.</li>
-          <li>Consultative Approach: I aim for a consulting role where I can harness my full potential, sincerity, and dedication, instead of a traditional employment model. My ideal work duration is 4-5 hours daily, not exceeding 100 hours monthly.</li>
-          <li>Working Hours: My productivity peaks during early morning shifts in the Indian timezone. If the job requires an 8-hour workday, I'd prefer flexible hours to maximize my efficiency.</li>
-          <li>Afternoon Constraints: I have personal commitments that make me unavailable post 1:00 PM IST, but I am readily available in the early mornings.</li>
-          <li>Compensation Structure: If considering a permanent role, please note I operate my consultation agency and manage my insurance and funds (NPF & PPF). I would appreciate an hourly compensation without additional benefits (Optional).</li>
-        </ol>
-      </div>
 
       <div className={` ${styles.paddingX} ${styles.flexStart}`}>
         <div className="services_container grid">
